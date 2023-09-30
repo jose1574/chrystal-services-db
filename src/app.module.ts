1d8219f30109d182bbb79c0d6be561ec261d621d
Jose Gomez <jose.gs1574@gmail.com>
2023-09-30 13:55:01 -0400

    se configuran las variables de entorno para datos de configuracion de bases de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
 import { SalesOperationModule } from './sales-operation/sales-operation.module';
 
+import { AppController } from './app.controller';
+
+import { AppService } from './app.service';
+
+
 @Module({
-  imports: [ DatabaseModule, ProductsModule, SalesOperationModule],
+  imports: [ 
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    DatabaseModule, 
+    ProductsModule,
+    SalesOperationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'root',
-            database: 'cadm_j295788193',
-            retryDelay:3000,
-            autoLoadEntities: true
-          }),
-    ]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        retryDelay: 3000,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
 })
 export class DatabaseModule {}
